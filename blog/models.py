5cdf89dba96d25015b8d0b81f93a7b4e91256c11
Abhimanyu Kumar <abhimanyukumar48@gmail.com>
2018-10-03 07:57:54 +0530

    Added blog templates

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.utils import timezone
 
+class Category(models.Model):
+    title = models.CharField(max_length=500)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=500)
@@ -14,3 +22,8 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Postcategorymap(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE)
+    category = models.ForeignKey('blog.Category', on_delete=models.CASCADE)
+    
--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_blogs, name = 'all_blogs'),
+    path('/<int:pk>/', views.blog_detail, name='blog_detail'),
 ]
